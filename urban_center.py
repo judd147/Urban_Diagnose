76a9de42dbcf9d33393d6319ba0aa33e829569dc
judd147 <57974800+judd147@users.noreply.github.com>
2022-05-27 15:14:28 +0800

    Update urban_center.py

--- urban_center.py
+++ urban_center.py
@@ -23,7 +23,7 @@ def main():
     st.sidebar.title("导航")
     apps = st.sidebar.multiselect("选择分析模块", ["城市中心体系分析"])
     
-    with open('D:\Users\zhangliyao\Desktop\Urban_Diagnose-main\config.yaml') as file:
+    with open(r'D:\Users\zhangliyao\Desktop\Urban_Diagnose-main\config.yaml') as file:
         config = yaml.load(file, Loader=SafeLoader)
     authenticator = stauth.Authenticate(
     config['credentials']['names'],
